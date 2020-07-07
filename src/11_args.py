d24d58daa1b9c18d422c97e398c5d5bca2e47fad
Scott Smith <scthsm@outlook.com>
2020-07-06 18:45:04 -0600

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(first,second):
+    return first + second
 
 print(f1(1, 2))
 
@@ -13,6 +15,14 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(num, *args):
+ if type(num) is not list:
+    total = num
+    for nums in args:
+        total = total + nums
+    return total
+ else: return sum(num)
+    
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -30,6 +40,14 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, *num2):
+    if not num2:
+        return num1 + 1
+
+    else:
+        return num1 + sum(num2)
+    
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +62,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**args):
+    for key,value in args.items():
+        print(f'key: {key}, value: {value}')
 
 # Should print
 # key: a, value: 12
@@ -62,4 +83,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
